70fa5468832665f00d19788e2c175358d6005868
Uddhav Zambare <zambare.u@northeastern.edu>
2022-12-02 01:46:16 -0500

    Adding System and Enterpise Admin Roles

---
+++ Final_Project/src/Business/Role/FNSEnterpriseAdmin.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Role;
+
+/**
+ *
+ * @author uddhavzambare
+ */
+
+//Center for Food Safety and Applied Nutrition
+public class FNSEnterpriseAdmin {
+    
+    @Override
+    public String toString(){
+        return "FNS Enterprise Admin";
+    }
+    
+}
+
+
