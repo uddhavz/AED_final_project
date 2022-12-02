77df147bff51fc5373c5b00fd316a0e020551e3d
Creed999 <sarode.r@northeastern.edu>
2022-12-02 18:21:15 -0500

    Added Order Product and Vendor

---
+++ Final_Project/src/Business/Configuration.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business;
+
+import Business.Employee.Emp;
+import Business.Role.SystemAdminRole;
+import Business.UserAccount.UserAccount;
+
+/**
+ *
+ * @author rajsarode
+ */
+public class Configuration {
+     
+}
